98f31b6077edf66137483652f13c4fec0060fdc6
CMS <chlalthd123@gamil.com>
2020-12-30 22:48:04 +0900

    variable

---
+++ java/Chapter2/src/variable/VariableTest.java
@@ -0,0 +1,20 @@
+package variable;
+
+public class VariableTest {
+	
+	public static void main(String[] args) {
+	
+		int age; //선언
+		
+		//오류는 빨간줄 경고는 노란줄
+		age = 30;
+		
+		System.out.println(age);
+		
+		age = 10;
+		
+		System.out.println(age);
+		
+		int count, num;
+	}
+}
--- java/First/src/hello/HelloJava.java
+++ java/First/src/hello/HelloJava.java
@@ -4,7 +4,6 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, Java");
-		//갓 시작한 것
 	}
 
 }
